0d35b9c22c61fdf12ad68744faca64e7da5ac646
Seung-Joon <tmdwns02556@gmail.com>
2020-05-23 23:15:35 +0900

    edit db table

--- todo/models.py
+++ todo/models.py
@@ -6,15 +6,15 @@
 #   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
 # Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
-from user.models import User
 
 
 class Todo(models.Model):
     todo_id = models.AutoField(primary_key=True)
-    user = models.OneToOneField(User, models.DO_NOTHING)
+    user_id = models.IntegerField()
     title = models.CharField(max_length=255)
     description = models.CharField(max_length=255)
     complete = models.IntegerField()
+    created_date = models.DateTimeField()
 
     class Meta:
         managed = False
